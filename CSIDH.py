6e8c11abd52b3e9aae73f241c52dac277e7e559e
Kostiantyn Boiar <opezdalya@gmail.com>
2024-11-07 11:51:18 -0600

    Alice and Bob are generating PK

--- CSIDH.py
+++ CSIDH.py
@@ -18,6 +18,7 @@ def public_params_generator(param):
             return E, P, Q  # Return the curve and the two points
     return None, None, None
 
+
 def find_point_of_order(curve, order):
     """Find a point on the elliptic curve with the specified order."""
     while 1:  
@@ -87,19 +88,16 @@ def compute_step(E, k, order):
     E_a1_j = E_a1.j_invariant()
     return S_isogeny, E_a1, E_a1_j
 
-def private_key_generation():
+
+def alice_step_public_gen():
     steps = []
-    # Generate public parameters P and Q for Bob and Alice on the same curve
-    E_bob, bob_public_p, bob_public_q = public_params_generator(choice(params))
     E_alice, alice_public_p, alice_public_q = public_params_generator(choice(params))
     
     # Check if valid public parameters are generated
     if not (E_alice and alice_public_p and alice_public_q):
         print("Error: Failed to generate valid public parameters for Alice.")
         return None
-    
-    print("Bob's public parameters: P_B =", bob_public_p, ", Q_B =", bob_public_q)
-    print("Alice's public parameters: P_A =", alice_public_p, ", Q_A =", alice_public_q)
+    print("Bob's public parameters: P_A =", alice_public_p, ", Q_A =", alice_public_q)
     
     # Alice's secret scalar
     k_0 = 11
@@ -131,7 +129,62 @@ def private_key_generation():
     print(f'Isogeny phi1 = {S_A_isogeny_3},\n EC_phi1 = {E_A_a1_3},\n j-inv_phi1 = {E_A_a1_j_3}')
     steps.append(E_A_a1_j_3)
 
-    print("Steps: ", steps)
+    final_E_alice, alice_public_p, alice_public_q = public_params_generator(E_A_a1_3.a4())
+    alice_PK = E_alice, alice_public_p, alice_public_q
+    return steps, alice_PK
 
+
+def bob_steps_public_gen():
+    steps = []
+    E_bob, bob_public_p, bob_public_q = public_params_generator(choice(params))
     
-private_key_generation()
+    # Check if valid public parameters are generated
+    if not (E_bob and bob_public_p and bob_public_q):
+        print("Error: Failed to generate valid public parameters for Bob.")
+        return None
+    print("Bob's public parameters: P_A =", bob_public_p, ", Q_A =", bob_public_q)
+    
+    # Alice's secret scalar
+    k_0 = 2
+    
+    #step 0
+    S_A_isogeny, E_A_a1, E_A_a1_j = compute_step(E_bob, k_0, 2)
+    print(f'Isogeny phi0 = {S_A_isogeny},\n EC_phi0 = {E_A_a1},\n j-inv_phi0 = {E_A_a1_j}')
+    steps.append(E_A_a1_j)
+    #step 1
+    k_1 = 5
+
+    S_A_isogeny_1, E_A_a1_1, E_A_a1_j_1 = compute_step(E_A_a1, k_1, 2)
+    print(f'Isogeny phi1 = {S_A_isogeny_1},\n EC_phi1 = {E_A_a1_1},\n j-inv_phi1 = {E_A_a1_j_1}')
+    steps.append(E_A_a1_j_1)
+
+
+    #step 2
+    k_2 = 9
+
+    S_A_isogeny_2, E_A_a1_2, E_A_a1_j_2 = compute_step(E_A_a1_1, k_2, 2)
+    print(f'Isogeny phi1 = {S_A_isogeny_2},\n EC_phi1 = {E_A_a1_2},\n j-inv_phi1 = {E_A_a1_j_2}')
+    steps.append(E_A_a1_j_2)
+
+
+    #step 3
+    k_3 = 3
+
+    S_A_isogeny_3, E_A_a1_3, E_A_a1_j_3 = compute_step(E_A_a1_2, k_3, 2)
+    print(f'Isogeny phi1 = {S_A_isogeny_3},\n EC_phi1 = {E_A_a1_3},\n j-inv_phi1 = {E_A_a1_j_3}')
+    steps.append(E_A_a1_j_3) 
+
+    final_E_bob, bob_public_p, bob_public_q = public_params_generator(E_A_a1_3.a4())
+    bob_PK = E_bob, bob_public_p, bob_public_q
+
+    return steps, bob_PK
+
+def public_key_generation():
+    steps = []
+    # Generate public parameters P and Q for Bob and Alice on the same curve
+    A_steps = alice_step_public_gen()
+    B_steps = bob_steps_public_gen()
+    print("Alice steps= ", A_steps)    
+    print("Bob steps= ", B_steps)
+
+public_key_generation()
